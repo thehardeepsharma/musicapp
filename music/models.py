b55c14d1cf3cf616bd7e1bf7dca9f516c467cda2
Hardeep Sharma <hardeeps@damcogroup.com>
2018-04-25 18:32:01 +0530

    project setup

---
+++ music/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Album(models.Model):
+	artist = models.CharField(max_length=250)
+	album_title = models.CharField(max_length=250)
+	genre = models.CharField(max_length=50)
+	album_logo = models.CharField(max_length=100)
+
+
+class Song(models.Model):
+	album = models.ForeignKey(Album, on_delete=models.CASCADE)
+	song_title = models.CharField(max_length=100)
+	file_type = models.CharField(max_length=10)

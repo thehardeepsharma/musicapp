9d9a9684af99bad6ff3f7a2448c4470495d73f32
Hardeep Sharma <thehardeepsharma@gmail.com>
2018-04-27 07:00:32 +0530

    Implement template shortcut

--- music/views.py
+++ music/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
-from django.template import loader
 from django.http import HttpResponse
 
 from .models import Album
 
 def index(request):
 	all_albums = Album.objects.all()
-	template = loader.get_template('music/index.html')
-	context = {
-		'all_albums': all_albums
-	}
-	return HttpResponse(template.render(context, request))
+	context = {'all_albums': all_albums}
+	return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):

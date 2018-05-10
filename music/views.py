1bb250ec44a39d98c45dc6103e354a007e37cbb5
Hardeep Sharma <hardeeps@damcogroup.com>
2018-05-10 16:22:42 +0530

    Implemented CreateView and Modal Form

--- music/views.py
+++ music/views.py
@@ -1,31 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Album, Song
 
-def index(request):
-	all_albums = Album.objects.all()
-	return render(request, 'music/index.html', {'all_albums': all_albums})
+class IndexView(ListView):
+	template_name = 'music/index.html'
+	context_object_name = 'all_albums'
+	def get_queryset(self):
+		return Album.objects.all()
 
 
-def detail(request, album_id):
-	# try:
-	# 	album = Album.objects.get(pk=album_id)
-	# except Album.DoesNotExist:
-	# 	raise Http404("Album does not exist ")
-	album = get_object_or_404(Album, pk=album_id)
-	return render(request, 'music/detail.html', {'album': album})
+class DetailView(DetailView):
+	model = Album
+	template_name = 'music/detail.html'
 
-def favorite(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
-	try:
-		selected_song = album.song_set.get(pk=request.POST['song'])
-	except (KeyError, Song.DoesNotExist):
-		return render(request, 'music/detail.html', {
-			'album': album,
-			'error_message': 'You did not select a valid song'
-		})
-	else:
-		selected_song.is_favorite = True
-		selected_song.save()
-		return render(request, 'music/detail.html', {'album': album})
+
+class AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist','album_title', 'genre','album_logo']

ce9e405ad0d529a292c84bfe82c2c704f8266047
Hardeep Sharma <hardeeps@damcogroup.com>
2018-05-15 13:22:17 +0530

    User registration, authentaticate, login

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
 	# /music/
 	url('^$', views.IndexView.as_view(), name='index'),
 
+	# /register/
+	url('^register/$', views.UserFormView.as_view(), name='register'),
+
 	# /music/album_id/
 	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 
--- music/views.py
+++ music/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .models import Album, Song
+from .forms import UserForm
 
 class IndexView(ListView):
 	template_name = 'music/index.html'
@@ -26,3 +29,29 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
 	model = Album
 	success_url = reverse_lazy('music:index')
+
+class UserFormView(View):
+	form_class = UserForm
+	template_name = 'music/registration_form.html'
+
+	# Display blank form
+	def get(self, request):
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form': form})
+
+	# Process data
+	def post(self, request):
+		form = self.form_class(request.POST)
+		user = form.save(commit=False)
+		username = form.cleaned_data['username']
+		password = form.cleaned_data['password']
+		user.set_password(password)
+		user.save()
+
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return redirect('music:index')
+
+		return render(request, self.template_name, {'form': form})
